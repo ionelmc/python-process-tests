0a12e6bc1d51b91d205532be19ef2733bb619f68
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-03-07 01:23:49 +0200

    Up version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 
 setup(
     name="process-tests",
-    version="0.4.3",
+    version="0.5.0",
     url='https://github.com/ionelmc/python-process-tests',
     download_url='',
     license='BSD',
