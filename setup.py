3a676b32371e8ec50c3574bf0cdb505e862cef34
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-01-16 02:40:11 +0200

    Bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name="process-tests",
-    version="2.0.0",
+    version="2.0.1",
     url='https://github.com/ionelmc/python-process-tests',
     download_url='',
     license='BSD',
